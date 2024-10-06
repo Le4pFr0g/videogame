981e304d7d9f2b91d7648302a08d174d2002012c
Le4pFr0g <149528833+Le4pFr0g@users.noreply.github.com>
2024-10-06 19:10:13 -0400

    Assignment2_2
    finished Assignment 2.2
    
    created a defaultStyles.css file for the top of the page styles that were the same

--- videogame/client/js/inputData.js
+++ videogame/client/js/inputData.js
@@ -12,3 +12,12 @@ function submitData()
 {
     alert("Submit button was pressed");
 }
+
+function clearData()
+{
+    $("#game_name").val("");
+    $("#year_released").val("");
+    $("#number_players").val("");
+    $("#game_platform").val("");
+    $("#rating").val("");
+}
---
+++ videogame/client/js/viewData.js
@@ -0,0 +1,60 @@
+var data = '[{"gameName": "game0", "yearReleased": "year0", "numPlayers": "Single player", "platform": "Desktop", "rating": "5"}]'
+
+//Create JSON object
+jsonObject = JSON.parse(data);
+
+
+//create temp games
+for (var i = 1; i < 5; i++)
+{
+    var newGame = {};
+    newGame.gameName = "game" + i;
+    newGame.yearReleased = "year" + i;
+    newGame.numPlayers = "Single Player";
+    newGame.platform = "Desktop";
+    newGame.rating = (5-i).toString();
+
+    jsonObject.push(newGame);
+}
+
+console.log(jsonObject);
+console.log(jsonObject.length);
+
+main();
+
+
+function main()
+{
+    loadData();
+}
+
+function loadData()
+{
+    var htmlString = "";
+
+    htmlString += "<tr>" +
+                    "<th>Game Name</th>" +
+                    "<th>Year Released</th>" +
+                    "<th>Player Type</th>" +
+                    "<th>Platform</th>" +
+                    "<th>Rating</th>"
+                    "</tr>";
+    for (var i = 0; i < jsonObject.length; i++)
+    {
+        htmlString += "<tr>";
+
+            htmlString += "<td>" + jsonObject[i].gameName + "</td>";
+            htmlString += "<td>" + jsonObject[i].yearReleased + "</td>";
+            htmlString += "<td>" + jsonObject[i].numPlayers + "</td>";
+            htmlString += "<td>" + jsonObject[i].platform + "</td>";
+            htmlString += "<td>" + jsonObject[i].rating + "</td>";
+            
+
+
+        
+        htmlString += "</tr>"
+    }
+
+    console.log(htmlString)
+    $("#dataTable").html(htmlString);
+}
